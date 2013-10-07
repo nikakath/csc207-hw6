08c213e7b724859b570cf0c71e64600777b4a6f0
KittNika <kittnika@gmail.com>
2013-10-07 16:42:15 -0500

    Deletions

--- work/gen/edu/grinnell/csc207/nikakath_byrnefio/hw6/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.grinnell.csc207.nikakath_byrnefio.hw6;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
