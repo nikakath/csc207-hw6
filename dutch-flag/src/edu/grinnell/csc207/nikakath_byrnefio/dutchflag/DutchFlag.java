1bbab1231fe1c42e39967fec83d76d4332388e2d
KittNika <kittnika@gmail.com>
2013-10-07 16:30:23 -0500

    Final Update

--- dutch-flag/src/edu/grinnell/csc207/nikakath_byrnefio/dutchflag/DutchFlag.java
+++ dutch-flag/src/edu/grinnell/csc207/nikakath_byrnefio/dutchflag/DutchFlag.java
@@ -2,20 +2,25 @@ package edu.grinnell.csc207.nikakath_byrnefio.dutchflag;
 
 import java.io.*;
 
+/** @author Kitt Nika & Fiona Byrne **/
+
 public class DutchFlag {
 	
 	public static void main(String[] args) throws Exception {
 		
+		/* Experiments! */
+		
 		int[] ugh = {0, 2, 1, 1, 2, 0, 0, 2, 1};
 		int[] nope = {};
 		int[] notToday = {1};
-	int[] notEver = {0, 1, 1, 0, 0, 0, 1};
+		int[] notEver = {0, 1, 1, 0, 0, 0, 1};
 		
 		flagSort(ugh);
 		flagSort(nope);
 		flagSort(notToday);
 		flagSort(notEver);
 		
+		/* Standard Case! */
 		PrintWriter pen = new PrintWriter(System.out, true);
 		pen.print("{");
 		for (int i = 0; i < ugh.length; i++) {
@@ -23,40 +28,52 @@ public class DutchFlag {
 		}
 		pen.println("}");
 
+		/* Null Case! */
 		pen.print("{");
 		for (int i = 0; i < nope.length; i++) {
 		pen.print(nope[i] + ",");
 		}
 		pen.println("}");
 
+		/* Singleton Case! */
 		pen.print("{");
 		for (int i = 0; i < notToday.length; i++) {
 		pen.print(notToday[i] + ",");
 		}
 		pen.println("}");
 		
-	pen.print("{");
+		/* Non-Standard Case! */
+		pen.print("{");
 		for (int i = 0; i < notEver.length; i++) {
 		pen.print(notEver[i] + ",");
 		}
 		pen.println("}");
 		
 		pen.close();
+		
 	} //main
 
+	/* Preconditions: Takes an array of integers whose members are all 0s, 1s, or 2s.
+	 * Each number does not need to be present.
+	 * Postconditions: The members of the array are sorted in ascending order.  The array
+	 * is mutated, and not returned. */
+	
 	public static void flagSort(int[] flag) {
 		
 		int red = 0;
-		int blue = 2;
+		int blue = 2;	
 		
 		for (int i = 0; i < flag.length; i++) {
-			
+		
 			int redbound = 0;
 			int bluebound = flag.length - 1;
 			
+			/* If flag[i] is not properly placed within the correct bounds, keep swapping
+			 * until it is. */
 			while ((flag[i] == red && i != redbound)
 					|| (flag[i] == blue && i != bluebound)) {
-				
+			
+				/* Place flag[i] at the appropriate boundary and increment that boundary */
 				if (flag[i] == red) {
 					swap(flag, i, redbound);
 					redbound++;
@@ -66,12 +83,14 @@ public class DutchFlag {
 					swap(flag, i, bluebound);
 					bluebound--;
 				} // if
-				
 			} // while
-			
 		} // for
-		
 	} // flagSort(int[])
+	
+	/* Preconditions: Takes an array of integers, and two indices within the bounds of the
+	 * array's length.
+	 * Postconditions: Swaps the values at the given indices within the array.  The array
+	 * is mutated, and not returned. */
 	
 	private static void swap(int[] array, int a, int b) {
 		
